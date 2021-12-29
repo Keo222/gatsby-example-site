9f5bfa764b9b240331ee94d048c7cc2043895dc9
Keo222 <kyote222@gmail.com>
2021-12-29 13:20:54 -0800

    finished part 2 of tutorial

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import Layout from '../components/Layout';
+
+function AboutPage() {
+    return (
+        <Layout pageTitle="About Page">
+            <p>Hi! I'm making my first Gatsby page.</p>
+        </Layout>
+    )
+}
+
+export default AboutPage
